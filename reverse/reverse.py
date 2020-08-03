db29a713c9c2403439e68db2e04cc1d65ddb5f3d
Tommy <tommykindle83@gmail.com>
2020-08-02 18:58:32 -0700

    finished reverse list with recursion

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,9 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node is not None:
+            next_node = node.get_next()
+            node.set_next(prev)
+            self.reverse_list(next_node, node)
+        else:
+            self.head = prev
